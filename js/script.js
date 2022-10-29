8c273ab0ac90286c200065462965d68766947edd
Muhammad Fadll <fadeljourney@gmail.com>
2022-10-29 10:36:26 +0700

    membuat beberapa function

--- js/script.js
+++ js/script.js
@@ -1,20 +1,98 @@
 const frontNumber = document.querySelector('.number_set');
 const frontName = document.querySelector(".name_set");
-const frontDate = document.querySelector(".year_set");
+const frontMonth = document.querySelector(".date_set .month");
+const frontYear = document.querySelector(".date_set .year");
+const frontCode = document.querySelector(".code_set");
 
-const inputName = document.querySelector("input[type=text]")
-const inputNumber = document.querySelector("input[type=number]")
-// const inputDate = document.querySelector("input[")
+const inputName = document.querySelector("input#username")
+const inputNumber = document.querySelector("input#number")
+const inputMonth = document.querySelector("input#month")
+const inputYear = document.querySelector("input#year")
+const inputCode = document.querySelector("input#code")
+const button = document.querySelector("button")
+
+// contoh
+
+inputNumber.value = "1234556677889900";
+inputName.value = "Muhammad Fadll"
 
 
-function createNumber() {
-    let value = inputNumber.value
-    frontNumber.innerHTML = value
+function createNumber(event) {
+    let value = inputNumber.value;
+    console.log(event.target == inputNumber)
+    handleInputNumber(value.split(''))
 }
 
-inputNumber.addEventListener('change', createNumber)
+function createUsername() {
+    let value = inputName.value;
+    frontName.innerHTML = value
+}
 
-// setInterval(function(){
-    
-// }, 10)
+function createMonth() {
+    let value = inputMonth.value;
+    frontMonth.innerHTML = value
+}
+function createYear() {
+    let value = inputYear.value;
+    frontYear.innerHTML = value
+}
+function createCode() {
+    let value = inputCode.value;
+    frontCode.innerHTML = value
+}
+
+function handleSubmit(e) {
+    e.preventDefault()
+    alert("succes")
+}
+
+
+
+function handleInputNumber(value) {
+    let arrayNumber = []
+    for (let i = 0; i < value.length; i++) {
+        if (i % 4 == 0 && i != 0) {
+            arrayNumber.push(' ')
+        }
+        arrayNumber.push(value[i])
+
+    }
+
+    arrayNumber = arrayNumber.join('')
+
+    frontNumber.innerHTML = arrayNumber
+
+    console.log(arrayNumber)
+    // checkNumberInput(arrayNumber)
+
+
+}
+
+inputName.addEventListener('change', createUsername)
+inputNumber.addEventListener('change', createNumber)
+inputMonth.addEventListener('change', createMonth)
+inputYear.addEventListener('change', createYear)
+inputCode.addEventListener('change', createCode)
+button.addEventListener('click', handleSubmit)
+
+
+
+// function checkNumberInput(number) {
+//     // let number = 'Mak3';
+
+//     let num = /[^0-9]/gi
+
+
+//     let result = number.match(num)
+
+//     console.log(result ? "harus angka semua" : "benar")
+// }
+
+
+
+// inputNumber.oninvalid = function (event) {
+//     event.target.setCustomValidity('Username should only contain lowercase letters. e.g. john');
+// }
+
+
 
