176887020d2f0a55f2cf88f873ce77dca15dc820
Muhammad Fadll <fadeljourney@gmail.com>
2022-11-06 20:20:21 +0700

    menyelesaikan script untuk pesan error

--- js/script.js
+++ js/script.js
@@ -13,19 +13,10 @@ const button = document.querySelector("button");
 
 const form = document.querySelector("form")
 const message = document.querySelector(".succes_message");
-// const allInput = document.getElementsByName('input')
 
 
 
-
-
-// contoh
-
-// inputNumber.value = "1234556677889900";
-// inputName.value = "Muhammad Fadll"
-
-
-function createNumber(event) {
+function createNumber() {
     let value = inputNumber.value;
     // console.log(event.target == inputNumber)
     handleInputNumber(value.split(''))
@@ -34,7 +25,7 @@ function createNumber(event) {
 function createUsername() {
     let value = inputName.value;
     frontName.innerHTML = value
-    // return value !== ""
+
 }
 
 function createMonth() {
@@ -51,42 +42,36 @@ function createCode() {
 }
 
 function handleSubmit() {
-
-    // const allInput = [inputName.value, inputNumber.value, inputMonth.value, inputYear.value, inputCode.value]
     const allInput = [inputName, inputNumber, inputMonth, inputYear, inputCode]
     const ID = [inputName.id, inputNumber.id, inputMonth.id, inputYear.id, inputCode.id]
 
-    // const checkInputs = allInput.filter(item => item != "")
-
     for (let i = 0; i < allInput.length; i++) {
         let item = allInput[i]
+        const regex = /[^0-9]/gi;
         if (item.value == "") {
             for (let j = i; j < ID.length; j++) {
 
                 if (item.id === ID[0]) {
                     item.nextElementSibling.innerHTML = "Username required"
-                } else if (item.id === ID[1]) {
-                    item.nextElementSibling.innerHTML = "Wrong format, number only"
-                } else {
+                }
+
+                else {
                     item.nextElementSibling.innerHTML = "can't be blank"
                 }
+
+                item.className = "error"
             }
 
-        } else {
+        } else if (allInput[1].value.match(regex)) {
+            allInput[1].nextElementSibling.innerHTML = "Wrong format, number only";
+            // console.log(allInput[1])
+        }
 
+        else {
+            item.classList.remove('error')
             item.nextElementSibling.innerHTML = ""
         }
     }
-
-
-
-    //     <p class="text_alert">Username required</p>
-    // <p class="text_alert">Wrong format, number only</p>
-    // <p class="text_alert">can't be blank</p>
-    // <p class="text_alert">can't be blank</p>
-
-
-    // console.log(checkInputs)
 }
 
 
@@ -130,10 +115,3 @@ button.addEventListener('click', handleSubmit)
 
 //     console.log(result ? "harus angka semua" : "benar")
 // }
-
-
-
-// inputNumber.oninvalid = function (event) {
-//     event.target.setCustomValidity('Username should only contain lowercase letters. e.g. john');
-// }
-
